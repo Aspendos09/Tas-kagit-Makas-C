8923e367d0a70974ea35a8259a1adf36dff83797
Aspendos09 <43933417+Aspendos09@users.noreply.github.com>
2018-10-21 01:21:00 +0300

    Sayaçlı versiyon.
    Kazanma,kaybetme ve beraberlik sayacı eklendi.

--- tkms.c
+++ tkms.c
@@ -4,45 +4,80 @@
 int main()
 {
 
-int hana,secim=0;
+int hana,secim=0 ,kazanma=0 ,kaybetme=0 ,beraberlik=0;
+srand(time(NULL));
+hana=rand()%2;/* tas=0 kagit=1 makas=2 */
+
 
 while(secim!=4)
 {
 printf("Tas Kagit Makas oyununa hos geldiniz. Lutfen hamlenizi seciniz(tas=1,kagit=2,makas=3,bitirmek icin 4 ). Bu arada Hana kim?\n ");	
 scanf("%d",&secim);	
 srand(time(NULL));
-hana=rand()%2;	/* tas=0 kagit=1 makas=2 */
+hana=rand()%2;	
 	if(hana==0) /*tas*/
 {
 	if(secim==1)
-	printf("Hana tas secti. Berabere kaldin.\n");	
-	
+	{
+		printf("Hana tas secti. Berabere kaldin.\n");	
+		beraberlik+=1;
+	}
 	if(secim==2)
-	printf("Hana tas secti. Helal reyiz kazandin.\n");	
+	{	
+		printf("Hana tas secti. Helal reyiz kazandin.\n");
+		kazanma+=1;
+	}	
 	if(secim==3)
-	printf("Hana tas secti. Kaybol yenildin.\n");	
+	{	
+		printf("Hana tas secti. Kaybol yenildin.\n");
+		kaybetme+=1;
+	}
+
 }	
 if(hana==1) /*kagit*/
 {
 	if(secim==1)
-	printf("Hana kagit secti. Kaybol yenildin.\n");	
-	
+	{	
+		printf("Hana kagit secti. Kaybol yenildin.\n");	
+		kaybetme+=1;
+	}
 	if(secim==2)
-	printf("Hana kagit secti. Berabere kaldin.\n");	
+	{	
+		printf("Hana kagit secti. Berabere kaldin.\n");	
+		beraberlik+=1;
+	}
 	if(secim==3)
-	printf("Hana kagit secti. Helal reyiz kazandin.\n" );	 
+	{
+		printf("Hana kagit secti. Helal reyiz kazandin.\n" );	 
+		kazanma+=1;
+	}
 }	
 if(hana==2) /*makas*/
 {
 	if(secim==1)
-	printf("Hana makas secti. Helal reyiz kazandin.\n");	
-	
+	{	
+		printf("Hana makas secti. Helal reyiz kazandin.\n");	
+		kazanma+=1;
+	}
 	if(secim==2)
-	printf("Hana makas secti. Kaybol yenildin.\n");	
+	{	
+		printf("Hana makas secti. Kaybol yenildin.\n");	
+		kaybetme+=1;
+	}	
 	if(secim==3)
-	printf("Hana tas secti. Berabere kaldin.\n");	
+	{	
+	printf("Hana tas secti. Berabere kaldin.\n");
+	beraberlik+=1;
+	}
+}	
+printf("\n");
+printf("Kazanma sayisi = %d ",kazanma);
+printf("Kaybetme sayisi = %d ",kaybetme);
+printf("Beraberlik sayisi = %d \n",beraberlik); 
+
 }
-}
+
+printf("\n");
 printf("Oyun Bitti\n");
 return 0;	
 }	
